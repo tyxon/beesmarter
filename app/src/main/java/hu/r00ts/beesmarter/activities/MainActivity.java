d7c477b24f187ee78b096c5a48dbd160828d8086
csubix2014 <csubix@caesar.elte.hu>
2015-02-21 18:58:50 +0100

    MainActivity elements

--- app/src/main/java/hu/r00ts/beesmarter/activities/MainActivity.java
+++ app/src/main/java/hu/r00ts/beesmarter/activities/MainActivity.java
@@ -2,14 +2,48 @@ package hu.r00ts.beesmarter.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.*;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import hu.r00ts.beesmarter.R;
 
 public class MainActivity extends Activity {
 
+    public EditText serverAddress;
+    public Button automaticTestButton;
+    public Button manualTestButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        serverAddress = (EditText) findViewById(R.id.serverAddress);
+        automaticTestButton = (Button) findViewById(R.id.automaticTestButton);
+        manualTestButton = (Button) findViewById(R.id.manualTestButton);
+
+        automaticTestButton.setOnClickListener(automaticTestButtonOnClickListener);
+        manualTestButton.setOnClickListener(manualTestButtonOnClickListener);
     }
+
+    OnClickListener automaticTestButtonOnClickListener = new OnClickListener(){
+
+            @Override
+            public void onClick(View arg0) {
+                Toast.makeText(MainActivity.this, serverAddress.getText(), Toast.LENGTH_SHORT);
+            }
+
+    };
+
+    OnClickListener manualTestButtonOnClickListener = new OnClickListener(){
+
+        @Override
+        public void onClick(View arg0) {
+
+        }
+
+    };
 }
