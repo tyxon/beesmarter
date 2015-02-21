15bfe1d7d6d1a907bb69debed98011ee3dc0ec9b
tyxon <tyxon92@gmail.com>
2015-02-21 19:34:52 +0100

    Activities

--- app/src/main/java/hu/r00ts/beesmarter/activities/SetPasswordActivity.java
+++ app/src/main/java/hu/r00ts/beesmarter/activities/SetPasswordActivity.java
@@ -4,15 +4,36 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import hu.r00ts.beesmarter.R;
 
 public class SetPasswordActivity extends Activity {
 
+    public EditText password;
+    public Button setPasswordButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_set_password);
+
+        password = (EditText) findViewById(R.id.password);
+        setPasswordButton = (Button) findViewById(R.id.setPasswordButton);
+
+        setPasswordButton.setOnClickListener(setPasswordButtonOnClickListener);
     }
+
+    View.OnClickListener setPasswordButtonOnClickListener = new View.OnClickListener(){
+
+        @Override
+        public void onClick(View arg0) {
+            Intent intent = new Intent(SetPasswordActivity.this, ManualTestActivity.class);
+            startActivity(intent);
+        }
+
+    };
 
 }
