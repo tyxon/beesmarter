458ac3c5148a52f5e2d4e26647ca65671e7644df
csubix2014 <csubix@caesar.elte.hu>
2015-02-21 18:44:14 +0100

    Client

---
+++ app/src/main/java/hu/r00ts/beesmarter/hu/r00ts/beesmarter/communication/Client.java
@@ -0,0 +1,67 @@
+package hu.r00ts.beesmarter.hu.r00ts.beesmarter.communication;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Client extends AsyncTask<Void, Void, Void> {
+
+    private String serverIP = "192.168.2.8";
+    private int serverPort = 9999;
+    private String response = "";
+    private Activity activity;
+
+    public Client(Activity activity){
+        this.activity = activity;
+    }
+
+    public Client(Activity activity, String serverIP){
+        this.activity = activity;
+        this.serverIP = serverIP;
+    }
+
+    @Override
+    protected Void doInBackground(Void... params) {
+        Socket socket = null;
+
+        try{
+            socket = new Socket(serverIP, serverPort);
+            ByteArrayOutputStream byteArrayOutputStream =new ByteArrayOutputStream(1024);
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            InputStream inputStream = socket.getInputStream();
+            while ((bytesRead = inputStream.read(buffer)) != -1){
+                byteArrayOutputStream.write(buffer, 0, bytesRead);
+                response += byteArrayOutputStream.toString("UTF-8");
+            }
+        } catch (UnknownHostException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex){
+            ex.printStackTrace();
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }finally{
+            if(socket != null){
+                try {
+                    socket.close();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void result) {
+        
+        super.onPostExecute(result);
+    }
+
+}
