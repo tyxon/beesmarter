5e8b943bf8eac42f43038156465f32fab0bbb3eb
tyxon <tyxon92@gmail.com>
2015-02-21 18:57:52 +0100

    MainActivity ManualTest button

--- app/src/main/java/hu/r00ts/beesmarter/activities/MainActivity.java
+++ app/src/main/java/hu/r00ts/beesmarter/activities/MainActivity.java
@@ -1,7 +1,9 @@
 package hu.r00ts.beesmarter.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import hu.r00ts.beesmarter.R;
 
@@ -11,5 +13,10 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void manualTest(View view) {
+        Intent intent = new Intent(this, ManualTestActivity.class);
+        startActivity(intent);
     }
 }
