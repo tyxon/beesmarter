db23c10fbf4dc8570d750680c7c80d39d40d5d09
tyxon <tyxon92@gmail.com>
2015-02-21 19:07:08 +0100

    SetPasswordActivity

---
+++ app/src/main/java/hu/r00ts/beesmarter/activities/SetPasswordActivity.java
@@ -0,0 +1,18 @@
+package hu.r00ts.beesmarter.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import hu.r00ts.beesmarter.R;
+
+public class SetPasswordActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
