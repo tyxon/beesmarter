e568307252241142b61f4b432075901a44ce54b0
tyxon <tyxon92@gmail.com>
2015-02-21 20:37:51 +0100

    CaseTest DrowDownList

--- app/src/main/java/hu/r00ts/beesmarter/activities/MainActivity.java
+++ app/src/main/java/hu/r00ts/beesmarter/activities/MainActivity.java
@@ -5,8 +5,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.*;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import hu.r00ts.beesmarter.R;
@@ -16,6 +18,7 @@ public class MainActivity extends Activity {
     public EditText serverAddress;
     public Button automaticTestButton;
     public Button manualTestButton;
+    public Spinner testCaseList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +28,12 @@ public class MainActivity extends Activity {
         serverAddress = (EditText) findViewById(R.id.serverAddress);
         automaticTestButton = (Button) findViewById(R.id.automaticTestButton);
         manualTestButton = (Button) findViewById(R.id.manualTestButton);
+        testCaseList = (Spinner) findViewById(R.id.testCase);
+
+        // set dropdownlist data
+        String[] items = new String[]{"TEST1", "TEST2", "TEST3", "TEST4"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, items);
+        testCaseList.setAdapter(adapter);
 
         automaticTestButton.setOnClickListener(automaticTestButtonOnClickListener);
         manualTestButton.setOnClickListener(manualTestButtonOnClickListener);
