45e942e005a3caa9fac059a14d62dc619f76e0fc
tyxon <tyxon92@gmail.com>
2015-02-21 18:42:08 +0100

    Added activities package
    MainActivity moved to activities package
    Added serverAddress inputBox and AutoTest Button

--- app/src/main/java/hu/r00ts/beesmarter/activities/MainActivity.java
+++ app/src/main/java/hu/r00ts/beesmarter/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package hu.r00ts.beesmarter;
+package hu.r00ts.beesmarter.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+
+import hu.r00ts.beesmarter.R;
 
 public class MainActivity extends Activity {
 
