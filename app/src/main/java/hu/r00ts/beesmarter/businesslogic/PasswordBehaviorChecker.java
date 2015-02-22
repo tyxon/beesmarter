d35f3b61259896d2d76ad3687c0a68f8bedd56ce
csubix2014 <csubix@caesar.elte.hu>
2015-02-22 09:53:28 +0100

    fix

--- app/src/main/java/hu/r00ts/beesmarter/businesslogic/PasswordBehaviorChecker.java
+++ app/src/main/java/hu/r00ts/beesmarter/businesslogic/PasswordBehaviorChecker.java
@@ -31,8 +31,8 @@ public class PasswordBehaviorChecker {
 
         constraints  = new ArrayList<>();
         constraints.add(new OverallTimeConstraint(training, pattern));
-        //constraints.add(new KeyPressedConstraint(training, pattern));
-        //constraints.add(new KeyReleasedConstraint(training, pattern));
+        constraints.add(new KeyPressedConstraint(training, pattern));
+        constraints.add(new KeyReleasedConstraint(training, pattern));
 
         double possibilities = 0;
         double sumWeights = 0;
