b47d3bf0e5a22823c2fbea59ef9ff3780f6ce063
tyxon <tyxon92@gmail.com>
2015-02-22 08:49:59 +0100

    OverallTimeConstraint
    threshold = minFullTime / 4;
    weight = 1

--- app/src/main/java/hu/r00ts/beesmarter/businesslogic/biometric/OverallTimeConstraint.java
+++ app/src/main/java/hu/r00ts/beesmarter/businesslogic/biometric/OverallTimeConstraint.java
@@ -1,10 +1,15 @@
 package hu.r00ts.beesmarter.businesslogic.biometric;
 
 
+import hu.r00ts.beesmarter.businesslogic.DTO.Key;
 import hu.r00ts.beesmarter.businesslogic.DTO.Pattern;
 import hu.r00ts.beesmarter.businesslogic.DTO.Training;
 
 public class OverallTimeConstraint extends BaseConstraint {
+
+    private Double threshold = 0d;
+    private Double minFullTime = 0d;
+    private Double maxFullTime = 0d;
 
     public OverallTimeConstraint(Training training, Pattern pattern) {
         super(training, pattern);
@@ -12,7 +17,47 @@ public class OverallTimeConstraint extends BaseConstraint {
 
     @Override
     public double getPossibility() {
-        return 1;
+        //threshold = getAvgKeyPressTime() * 2;
+        setMinMaxOverallTime();
+        threshold = minFullTime / 4;
+
+        Double patternFullTime = pattern.getFullTime().doubleValue();
+        if (patternFullTime >= minFullTime && patternFullTime <= maxFullTime) {
+            return 1 * getWeight();
+        } else if (patternFullTime >= minFullTime - threshold) {
+            return ((minFullTime - threshold) / patternFullTime) * getWeight();
+        } else if (patternFullTime <= maxFullTime + threshold) {
+            return (patternFullTime / (maxFullTime + threshold)) * getWeight();
+        } else {
+            return 0.1 * getWeight();
+        }
+    }
+
+
+    private Double getAvgKeyPressTime() {
+        Double avgKeyPressTime = 0d;
+        for (Pattern pattern : training.Patterns) {
+            Double avgKeyPressTimePerPattern = 0d;
+            for (Key key : pattern.Keys) {
+                avgKeyPressTimePerPattern += key.getKeyPressTime().doubleValue();
+            }
+            avgKeyPressTime += avgKeyPressTimePerPattern / pattern.Keys.size();
+        }
+
+        return avgKeyPressTime / training.Patterns.size();
+    }
+
+    private void setMinMaxOverallTime() {
+        for (Pattern pattern : training.Patterns) {
+            Double fullTime = pattern.getFullTime().doubleValue();
+            if (minFullTime == 0d || minFullTime > fullTime) {
+                minFullTime = fullTime;
+            }
+
+            if (maxFullTime < fullTime) {
+                maxFullTime = fullTime;
+            }
+        }
     }
 
 }
